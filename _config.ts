58f67f9e5305e9301b1be87ba775302a8884d84b
Matthew Stibbard <matthew@stibbard.io>
2024-12-24 14:40:18 +1100

    add tailwind. siteConfig in _data

--- _config.ts
+++ _config.ts
@@ -1,5 +1,15 @@
 import lume from "lume/mod.ts";
+import postcss from "lume/plugins/postcss.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import typography from "npm:@tailwindcss/typography";
 
 const site = lume({ src: "./src" });
+site.use(tailwindcss({
+  extensions: [".html", ".tsx"],
+  options: {
+    plugins: [typography],
+  },
+}));
+site.use(postcss());
 
 export default site;
